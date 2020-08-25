1f3e46bb95604c86ca0bb5d0b20a6856af211d96
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-25 04:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocknum){
+  return Math.abs(42-blocknum)
+}
+function distanceFromHqInFeet(blocknum){
+  return distanceFromHqInBlocks(blocknum)*264
+}
+function distanceTravelledInFeet(stat, end){
+  return Math.abs((end-stat)*(264))
+}
+function calculatesFarePrice(start, destination){
+  ler totalfeet = distanceTravelledInFeet(start, destination)
+  case totalfeet < 400:
+    return 0 
+  case totalfeet>=400 && totalfeet<=2000
+    return totalfeet * 2
+  case totalfeet >2000
+    return 25
+  case totalfeet > 2500 
+    return 'No Ride'
+}
